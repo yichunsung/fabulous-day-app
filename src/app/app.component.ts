3053736a27e656868f0fd283a5d3a9ad1da2bb72
YiChun Sung <ycsung.r@gmail.com>
2020-10-24 03:16:31 +0800

    Testing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+// Constant
+import { mrt } from './mrt-data.const';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fabulous-day';
+  title = 'fabulous day';
+  list = mrt;
 }
