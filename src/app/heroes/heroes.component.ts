d3eced12b2171d6a384188395069e22ff6f841d7
YiChun Sung <ycsung.r@gmail.com>
2021-02-03 03:25:44 +0800

    Added hero component.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+	party = 'party';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
