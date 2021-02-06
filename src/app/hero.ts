9a88414bfa6cc7d094b814ef0320690c4b0afb4e
YiChun Sung <ycsung.r@gmail.com>
2021-02-07 04:07:08 +0800

    Added images.

---
+++ src/app/hero.ts
@@ -0,0 +1,6 @@
+export interface Hero {
+	id: number,
+	name: string,
+	role: string,
+	photo: string
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { Hero } from '../hero';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-	party = 'party';
-
-  constructor() { }
+	party = 'Hello Party';
+  pageName: string = '';
+  heroVal: any = 'asds';
+	heroList: Hero[] = [
+    {
+      id: 1,
+      name: '芳根京子',
+      role: 'range 1',
+      photo: 'https://pbs.twimg.com/media/EtHar4nU0AI1J8p?format=jpg&name=large'
+    },
+    {
+      id: 2,
+      name: '堀田真由',
+      role: 'range 1',
+      photo: 'https://pbs.twimg.com/media/EsLTOA8VQAQXUGt?format=jpg&name=large'
+    },
+    {
+      id: 3,
+      name: 'milet',
+      role: 'range 1',
+      photo: 'https://pbs.twimg.com/media/Eqe310NUUAA7jyw?format=jpg&name=large'
+    },
+    {
+      id: 4,
+      name: '飯豊 まりえ',
+      role: 'range 1',
+      photo: 'https://pbs.twimg.com/media/DZmwMH5VoAE0bmH?format=jpg&name=large'
+    },
+  ];
 
   ngOnInit(): void {
+    this.pageName = 'Hero Page';
+  }
+
+  show(): void {
+    console.log(this.party);
+    console.log(this.heroVal);
+    this.pageName = this.pageName === 'Hello Party' ? 'Oh yoyoy' : this.party;
   }
 
 }
