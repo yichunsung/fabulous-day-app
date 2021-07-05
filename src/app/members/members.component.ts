572539c56b62b58ba86220bc5433b1d97140b324
YiChun Sung <ycsung.r@gmail.com>
2021-07-06 01:49:04 +0800

    Added RxJS in API call.

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 class MemberData {
 	name: string;
 	email: string;
 	role: string;
+}
+
+interface Organization {
+  id: number;
+  company_name: string;
+  building_date: string;
+  address: string;
+}
+
+interface APIReturn {
+  status: number;
+  message: string;
+  data: Organization[];
 }
 
 @Component({
@@ -13,7 +28,9 @@ class MemberData {
 })
 export class MembersComponent implements OnInit {
 
-  membersList: MemberData[] = [
+  private apiUrl: string = 'https://fable-api.elk-tree.site/api/organizations';
+
+  public membersList: MemberData[] = [
     {
       name: 'Roy',
       email: 'ycsung.r@elk-tree.studio',
@@ -31,10 +48,25 @@ export class MembersComponent implements OnInit {
     }
   ];
 
-  constructor() { }
+  public organizationData: Organization = {
+    id: 0,
+    company_name: '',
+    building_date: '',
+    address: ''
+  };
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-	 console.log('MembersPage')
+	 console.log('MembersPage');
+   this.getDataFromAPI().subscribe((res) => {
+    this.organizationData = res.data[0];
+    console.log(res.data[0]);
+   });
   }
 
+  getDataFromAPI(): Observable<APIReturn> {
+    console.log(this.apiUrl);
+    return this.http.get<APIReturn>(this.apiUrl);
+  }
 }
